906995652997244425028be7f6f2492f6903a1b2
waliddx <wagzouly@gmail.com>
2024-05-28 07:27:36 -0700

    Back-end

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 "creating the route app"
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 """adding simple route that
     lead to the index html file located
@@ -18,4 +19,4 @@ def home():
 
 "app runner"
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
