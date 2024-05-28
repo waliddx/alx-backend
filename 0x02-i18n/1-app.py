636ad9936e94c39329000037c835816483cd0847
waliddx <wagzouly@gmail.com>
2024-05-28 07:40:52 -0700

    Back-end

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+
+from flask_babel import Babel
+from flask import Flask, render_template
+
+class config:
+    """Represent the flask babel config.
+    """
+    LANGUAGE = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index():
+    """The home/index page.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
