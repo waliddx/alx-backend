e63e983a0f557ae1a65e10ee5735e51bbcb7f6f2
waliddx <wagzouly@gmail.com>
2024-05-28 07:17:47 -0700

    Back-end

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python3
+
+"importing flask"
+
 from flask import Flask, render_template
 
+"creating the route app"
+
 app = Flask(__name__)
+
+"""adding simple route that
+    lead to the index html file located
+        in templates by using render_temlpate"""
 
 @app.route("/")
 def home():
     return render_template("0-index.html")
 
+"app runner"
 if __name__ == "__main__":
     app.run(debug=True)
